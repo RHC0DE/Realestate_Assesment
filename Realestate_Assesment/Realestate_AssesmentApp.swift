2d74b478743161fc38c44d952fb105dd9f250a68
R H <96940361+RHC0DE@users.noreply.github.com>
2023-07-03 16:58:21 +0200

    Initial Commit

---
+++ Realestate_Assesment/Realestate_AssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realestate_AssesmentApp.swift
+//  Realestate_Assesment
+//
+//  Created by Guest User on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Realestate_AssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
